f7c4adccae3214b188bb02636f68667f955d705d
Abraham Benski <AbrahamBenski@proton.me>
2025-03-25 18:15:36 -0500

    create content/; add generate_page()

--- src/main.py
+++ src/main.py
@@ -13,11 +13,12 @@ from shutil import (
 # )
 
 from markdownblock import extract_title
+from textmarkdown import markdown_to_html_node
 
 
 def main():
-    source = "static"
-    destination = "public"
+    source: str = "static"
+    destination: str = "public"
 
     # If user reverses the args, they could mistakenly delete all the static
     # files they prepared for this job. Make sure they want this. Currently
@@ -30,9 +31,12 @@ def main():
     #     print("Exiting program without copying...")
     #     exit(0)
 
-    title = extract_title("# title")
-    print(title)
     static_to_public(source, destination)
+
+    from_path: str = "content/index.md"
+    template_path: str = "template.html"
+    dest_path: str = "public/index.html"
+    generate_page(from_path, template_path, dest_path)
 
 
 # Copies the contents of /statis to /public after deleting everything in
@@ -51,23 +55,50 @@ def static_to_public(src: str, dst: str) -> None:
     # item to see if it's a directory for mkdir() or file for copy(). We'll
     # do this recursively, so here's a helper function to avoid re-making
     # our destination directory over and over.
+    def copy_helper(src: str, dst: str) -> None:
+        items: list[str] = listdir(src)
+        for item in items:
+            current_src: str = path.join(src, item)
+            current_dst: str = path.join(dst, item)
+            if path.isfile(current_src):
+                print(f"Found file. Copying {current_src} to {current_dst}")
+                copy(current_src, current_dst)
+            else:
+                print(f"Found directory. Creating {current_dst}")
+                mkdir(current_dst)
+                print(f"Starting crawl through next directory {current_src}")
+                copy_helper(current_src, current_dst)
+
     print(f"Starting crawl through source {src}...")
     copy_helper(src, dst)
 
 
-def copy_helper(src: str, dst: str) -> None:
-    items: list[str] = listdir(src)
-    for item in items:
-        current_src: str = path.join(src, item)
-        current_dst: str = path.join(dst, item)
-        if path.isfile(current_src):
-            print(f"Found file. Copying {current_src} to {current_dst}")
-            copy(current_src, current_dst)
-        else:
-            print(f"Found directory. Creating {current_dst}")
-            mkdir(current_dst)
-            print(f"Starting crawl through next directory {current_src}")
-            copy_helper(current_src, current_dst)
+# Reads the markdown file at from_path and, using the template, populates the
+# {{ Content }} tag with HTML generated using the markdown document.
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} \
+            using {template_path}")
+
+    with open(from_path, "r") as f:
+        md = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    content = markdown_to_html_node(md).to_html()
+    title = extract_title(md)
+    with_title = template.replace("{{ Title }}", title)
+    html = with_title.replace("{{ Content }}", content)
+
+    current_path = ""
+    for dir in dest_path.split("/"):
+        if dir == "":
+            continue
+        current_path += dir
+        if not path.exists(current_path):
+            mkdir(current_path)
+
+    with open(dest_path, "w") as f:
+        f.write(html)
 
 
 if __name__ == "__main__":
